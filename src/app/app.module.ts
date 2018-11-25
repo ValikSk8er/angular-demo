4dfc25d440d111f4e6d84efe7f2f66a3c48aa5a4
ValikSk8er <valiksk8@gmail.com>
2018-11-25 16:10:47 +0200

    registration page was added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoriesComponent } from './categories/categories.component';
+import {CategoryService} from './services/category.service';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule, Routes} from '@angular/router';
+import { RegistrationComponent } from './registration/registration.component';
+import {FormsModule} from '@angular/forms';
+
+const appRouts: Routes = [
+  {path: 'categories', component: CategoriesComponent},
+  {path: 'registration', component: RegistrationComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriesComponent,
+    RegistrationComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRouts
+      ),
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CategoryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
